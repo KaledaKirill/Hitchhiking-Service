de3852a8bdaba29ad9f8b7ee47e620302b0ff770
Kirill <kirill.kaleda16@gmail.com>
2025-03-18 10:57:52 +0300

    Fix Sonar

--- src/main/java/com/example/hitchhikingservice/repository/UserRepository.java
+++ src/main/java/com/example/hitchhikingservice/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.example.hitchhikingservice.repository;
 
 import com.example.hitchhikingservice.model.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByName(String name);
+    boolean existsByName(String name);
 }
